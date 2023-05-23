945651d81823e33ec01fc1c77252a054b3d81d5a
Ryouma Seto <2023php02-student023@la-study.com>
2023-05-23 15:04:12 +0900

    homeとlendingのindex画面に修正を加えています。

--- Library/resources/views/home/index.blade.php
+++ Library/resources/views/home/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<p><a href="customers/index">会員管理画面</a></p>
-<p><a href="books/index">書籍管理画面</a></p>
-<p><a href="lendings/index">貸出台帳画面</a></p>
+<p><a href="{{ route('customers.index') }}">会員管理画面</a></p>
+<p><a href="{{ route('books.index') }}">資料管理画面</a></p>
+<p><a href="{{ route('lendings.index') }}">貸出台帳画面</a></p>
 
 <form action="{{ route('logout') }}" method="POST">
     @csrf
