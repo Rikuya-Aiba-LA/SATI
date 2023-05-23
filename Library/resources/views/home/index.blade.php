d98dd0934f99dd964852be3ef91f4fd3ed29bd53
Mariko Takano <2023php02-student006@la-study.com>
2023-05-23 14:41:13 +0900

    ログアウトボタン付けなおし

--- Library/resources/views/home/index.blade.php
+++ Library/resources/views/home/index.blade.php
@@ -3,5 +3,10 @@
 @section('content')
 <p><a href="customers/index">会員管理画面</a></p>
 <p><a href="books/index">書籍管理画面</a></p>
-<p><a href="lendings/index">画面</a></p>
+<p><a href="lendings/index">貸出台帳画面</a></p>
+
+<form action="{{ route('logout') }}" method="POST">
+    @csrf
+    <input type="submit" value="ログアウト">
+</form>
 @endsection
