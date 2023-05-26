5d94178cdeaa291534845daf97bee302969c3751
Mariko Takano <2023php02-student006@la-study.com>
2023-05-26 11:54:09 +0900

    メニュー画面のレイアウト作成

--- Library/resources/views/home/index.blade.php
+++ Library/resources/views/home/index.blade.php
@@ -5,17 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-    
-<p><a href="{{ route('customers.index') }}">会員管理画面</a></p>
-<p><a href="{{ route('books.index') }}">資料管理画面</a></p>
-<p><a href="{{ route('lendings.index') }}">貸出台帳画面</a></p>
-
+    <section class="menu">
+<p class="title">{{ config('app.name') }}</p>
+<div class="flex">
+<p class="element"><a href="{{ route('customers.index') }}">会員管理画面</a></p>
+<p class="element"><a href="{{ route('books.index') }}">資料管理画面</a></p>
+<p class="element"><a href="{{ route('lendings.index') }}">貸出台帳画面</a></p>
+</div>
 <form action="{{ route('logout') }}" method="POST">
     @csrf
     <input type="submit" value="ログアウト">
 </form>
-
+</section>
 </body>
 </html>
