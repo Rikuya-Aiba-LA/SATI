3ab0ca847c9f5533313291ed7e34f37779291a88
shu imai <2023php02-student011@la-study.com>
2023-05-26 16:37:02 +0900

    a

--- Library/resources/views/lendings/check.blade.php
+++ Library/resources/views/lendings/check.blade.php
@@ -4,7 +4,7 @@
 <h1>貸出確認画面</h1>
 @if($book)
   <h2>会員情報</h2>
-  <form action="{{ route('lendings.store', $customer->id) }}" method="post">
+  <form action="{{ route('lendings.store', $customer->id) }}" method="post" name="contact_form">
     @csrf
   <dl>
     <dt>会員ID</dt>
@@ -54,8 +54,33 @@
   <h2>返却予定日</h2>
     <p>{{ $expectied_date }}</p>
     <input type="hidden" name="expectied_date" value="{{ $expectied_date }}">
-    <input type="submit" value="貸出する">
+    <button onclick="lendBook()">貸出する</button>
   </form>
+  <script>
+        //[確認]ボタンが押されたときの処理を定義
+       function lendBook() {
+            //input要素の入力内容を取得
+            const id = "{{$customer->id}}";
+            const name = "{{$customer->name}}";
+            const book_id = "{{$book->id}}";
+            const title = "{{$book->title}}";
+            const author = "{{$book->author}}";
+            const publisher = "{{$book->publisher}}";
+            const expectied_date = "{{$expectied_date}}";
+            event.preventDefault();
+            if (confirm('以下の本を返却しますか？\n' + "会員ID:" + id + "\n"
+                    + "氏名:" + name +"\n"
+                    + "資料ID:" + book_id +"\n"
+                    + "資料名:" + title + "\n"
+                    + "著者:" + author + "\n"
+                    + "出版社:" + publisher + "\n"
+                    + "返却予定日:" + expectied_date )
+        ) {
+                contact_form.submit();
+            }
+        }
+        ;
+    </script>
   @endif
 @else
   <p>該当する資料が存在しません</p>
