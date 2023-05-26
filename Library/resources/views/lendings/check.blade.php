ec3535a5b203f4f87fb7a3329aa316604b281146
Rikuya-Aiba-LA <133730436+Rikuya-Aiba-LA@users.noreply.github.com>
2023-05-26 16:12:53 +0900

    Revert "退会、廃棄、貸出、返却ボタンを押した際のポップアップ表示の追加"

--- Library/resources/views/lendings/check.blade.php
+++ Library/resources/views/lendings/check.blade.php
@@ -4,7 +4,7 @@
 <h1>貸出確認画面</h1>
 @if($book)
   <h2>会員情報</h2>
-  <form action="{{ route('lendings.store', $customer->id) }}" method="post" name="contact_form">
+  <form action="{{ route('lendings.store', $customer->id) }}" method="post">
     @csrf
   <dl>
     <dt>会員ID</dt>
@@ -54,33 +54,8 @@
   <h2>返却予定日</h2>
     <p>{{ $expectied_date }}</p>
     <input type="hidden" name="expectied_date" value="{{ $expectied_date }}">
-    <button onclick="lendBook()">貸出する</button>
+    <input type="submit" value="貸出する">
   </form>
-  <script>
-        //[確認]ボタンが押されたときの処理を定義
-       function lendBook() {
-            //input要素の入力内容を取得
-            const id = "{{$customer->id}}";
-            const name = "{{$customer->name}}";
-            const book_id = "{{$book->id}}";
-            const title = "{{$book->title}}";
-            const author = "{{$book->author}}";
-            const publisher = "{{$book->publisher}}";
-            const expectied_date = "{{$expectied_date}}";
-            event.preventDefault();
-            if (confirm('以下の本を返却しますか？\n' + "会員ID:" + id + "\n"
-                    + "氏名:" + name +"\n"
-                    + "資料ID:" + book_id +"\n"
-                    + "資料名:" + title + "\n"
-                    + "著者:" + author + "\n"
-                    + "出版社:" + publisher + "\n"
-                    + "返却予定日:" + expectied_date )
-        ) {
-                contact_form.submit();
-            }
-        }
-        ;
-    </script>
   @endif
 @else
   <p>該当する資料が存在しません</p>
