22291b18cc00aa477ff521a5b67562b5da3d5541
Rikuya Aiba <2023php02-student014@la-study.com>
2023-05-26 14:16:23 +0900

    在庫資料一覧か廃棄済み資料か、どちらを表示しているのかを分かりやすくしました

--- Library/resources/views/books/index.blade.php
+++ Library/resources/views/books/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts/app')
 
 @section('content')
-  <h1>資料管理</h1>
+  @if($books->whereNotNull('trash_date')->count() > 0)
+    <h1>廃棄済み資料</h1>
+  @else
+    <h1>在庫資料一覧</h1>
+  @endif
   <button onclick="location.href='{{ route('books.create') }}'">新規登録</button>
   <form action="{{ route('books.index') }}" method="get">
     <input type="number" name="id" value="{{ request('id') }}" placeholder="資料ID" required>
@@ -9,7 +13,11 @@
   </form>
   <form action="{{ route('books.index') }}" method="get">
     <input type="hidden" name="trash_date" value="日本語">
-    <input type="submit" value="廃棄済み資料">
+    <input type="submit" onclick="changeTrash()" value="廃棄済み資料">
+  </form>
+  <form action="{{ route('books.index') }}" method="get">
+    <input type="hidden" name="id" value = ''>
+    <input type="submit" onclick="changeExists()" value="在庫資料一覧">
   </form>
   <hr>
   @if($books->count() == 0)
@@ -22,7 +30,9 @@
           <th>資料名</th>
           <th>著者</th>
           <th>出版日</th>
-          <th>廃棄年月日</th>
+          @if($books->whereNotNull('trash_date')->count() > 0)
+            <th>廃棄年月日</th>
+          @endif
         </tr>
       </thead>
       @foreach($books as $book)
@@ -39,16 +49,10 @@
               preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}/',$book->trash_date, $trash_date_match);
             ?>
             <td>{{ $trash_date_match[0] }}</td>
-          @else
-            <td></td>
           @endif
         </tr>
       @endforeach
     </table>
     {{ $books->links() }}
   @endif
-  <form action="{{ route('books.index') }}" method="get">
-    <input type="hidden" name="id" value = ''>
-    <input type="submit" value="一覧で表示する">
-  </form>
 @endsection
